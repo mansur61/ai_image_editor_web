c6510224209110a402f2ffc954458a8c2ad321cc
Mansur Emin Kaya <kayamansur61@gmail.com>
2025-10-08 12:02:29 +0300

    test hatalı düzenlendi

--- fronted/test/widget_test.dart
+++ fronted/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fronted/main_1.dart';
+import 'package:fronted/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
